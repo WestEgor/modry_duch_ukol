84dcd0a45766201bff1faa49c02e0d769b44ca74
WestEgor <davidpayne.west@gmail.com>
2022-02-28 00:52:21 +0100

    Přidano mazání kontaktů a dokumentace

--- src/Controller/KontaktyController.php
+++ src/Controller/KontaktyController.php
@@ -7,27 +7,26 @@ use App\Form\Type\KontaktyType;
 use App\Repository\KontaktyRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Třída pro práci s kontakty
+ *
+ * Class KontaktyController
+ * @package App\Controller
+ */
 class KontaktyController extends AbstractController
 {
 
-    private RequestStack $requestStack;
-
     /**
-     * KontaktyController constructor.
-     * @param RequestStack $requestStack
+     * Zobrázení všech kontaktů
+     *
+     * @param KontaktyRepository $kontaktyRepository
+     * @return Response
      */
-    public function __construct(RequestStack $requestStack)
-    {
-        $this->requestStack = $requestStack;
-    }
-
-
     #[Route('/', name: 'kontakty')]
     public function index(KontaktyRepository $kontaktyRepository): Response
     {
@@ -37,20 +36,32 @@ class KontaktyController extends AbstractController
         ]);
     }
 
+    /**
+     * Editování zvoleného kontaktu
+     *
+     * @param ManagerRegistry $doctrine
+     * @param Request $request
+     * @param string $celeJmeno
+     * @return Response
+     */
     #[Route('/identifikator-kontaktu/{celeJmeno}', name: 'identifikator-kontaktu', methods: ['post', 'get'])]
     public function editovatKontakt(ManagerRegistry $doctrine, Request $request, string $celeJmeno): Response
     {
+        /** Nastavení session pro id kontaktu */
         if (!$request->getSession()->get('id_kontakt')) {
+            /** @var int $kontaktId bereme z formy, kontakt/index.html.twig */
             $kontaktId = (int)$request->get('kontakt-id');
             $request->getSession()->set('id_kontakt', $kontaktId);
         }
         $entityManager = $doctrine->getManager();
+        /** @var Kontakty $kontakt nalézání kontaktu dle ID */
         $kontakt = $entityManager->getRepository(Kontakty::class)
             ->find($request->getSession()->get('id_kontakt'));
         $form = $this->createForm(KontaktyType::class, $kontakt);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
+            /** vymazat id kontaktu z session*/
             $request->getSession()->remove('id_kontakt');
             return $this->redirectToRoute('kontakty');
         }
@@ -58,5 +69,26 @@ class KontaktyController extends AbstractController
         return $this->render('kontakt/editovani.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+
+    /**
+     * Jednoduché mazání kontaktu
+     *
+     * @param ManagerRegistry $doctrine
+     * @param int $id
+     * @return Response
+     */
+    #[ROUTE('/delete_article/{id}', name: 'smazat-kontakt', methods: ['get'])]
+    public function smazatKontakt(ManagerRegistry $doctrine, int $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+        /** @var Kontakty $kontakt nalézání kontaktu dle ID */
+        $kontakt = $entityManager->getRepository(Kontakty::class)->find($id);
+        $entityManager = $doctrine->getManager();
+        if ($kontakt !== null) {
+            $entityManager->remove($kontakt);
+        }
+        $entityManager->flush();
+        return $this->redirectToRoute('kontakty');
     }
 }
--- src/Entity/Kontakty.php
+++ src/Entity/Kontakty.php
@@ -8,40 +8,64 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: KontaktyRepository::class)]
 class Kontakty
 {
+    /**
+     * @var int $id
+     */
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
     private $id;
 
+    /**
+     * Jméno a příjmení kontaktu
+     * @var string $celeJmeno
+     */
     #[ORM\Column(type: 'string', length: 100)]
-    private $celeJmeno;
+    private string $celeJmeno;
 
-    #[ORM\Column(type: 'string', length: 25)]
+    /**
+     * Telefonní číslo kontaktu
+     * Musí být unikatním
+     * @var string $telefonniCislo
+     */
+    #[ORM\Column(type: 'string', length: 25, unique: true)]
     private $telefonniCislo;
 
+    /**
+     * Email kontaktu
+     * @var string $email
+     */
     #[ORM\Column(type: 'string', length: 100)]
-    private $email;
+    private string $email;
 
+    /**
+     * Dlouhá poznámka ke kontaktu
+     * @var string $poznamka
+     */
     #[ORM\Column(type: 'text')]
-    private $poznamka;
+    private string $poznamka;
 
-    public function setId(int $id): self
-    {
-        $this->id = $id;
 
-        return $this;
-    }
-
+    /**
+     * @return int|null
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    /**
+     * @return string|null
+     */
     public function getCeleJmeno(): ?string
     {
         return $this->celeJmeno;
     }
 
+    /**
+     * @param string $celeJmeno
+     * @return $this
+     */
     public function setCeleJmeno(string $celeJmeno): self
     {
         $this->celeJmeno = $celeJmeno;
@@ -49,11 +73,18 @@ class Kontakty
         return $this;
     }
 
+    /**
+     * @return string|null
+     */
     public function getTelefonniCislo(): ?string
     {
         return $this->telefonniCislo;
     }
 
+    /**
+     * @param string $telefonniCislo
+     * @return $this
+     */
     public function setTelefonniCislo(string $telefonniCislo): self
     {
         $this->telefonniCislo = $telefonniCislo;
@@ -61,11 +92,18 @@ class Kontakty
         return $this;
     }
 
+    /**
+     * @return string|null
+     */
     public function getEmail(): ?string
     {
         return $this->email;
     }
 
+    /**
+     * @param string $email
+     * @return $this
+     */
     public function setEmail(string $email): self
     {
         $this->email = $email;
@@ -73,11 +111,18 @@ class Kontakty
         return $this;
     }
 
+    /**
+     * @return string|null
+     */
     public function getPoznamka(): ?string
     {
         return $this->poznamka;
     }
 
+    /**
+     * @param string $poznamka
+     * @return $this
+     */
     public function setPoznamka(string $poznamka): self
     {
         $this->poznamka = $poznamka;
